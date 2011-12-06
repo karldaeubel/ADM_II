524063e75f010b58f21171eb6e4044d5a96689e2
Karl Daeubel <karl_daeubel@web.de>
2011-12-07 00:39:09 +0100

    Java Project

---
+++ src/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author Karl Daeubel
+ * A class for testing the GitHub-Repository with Eclipse and EGit
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hallo Git!");
+	}
+}
