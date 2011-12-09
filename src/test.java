66d4c1e6d4dd61aee89953f00e2c793c5b0618a1
karldaeubel <lrak_lebuet@web.de>
2011-12-09 13:59:01 +0100

    Update src/test.java

--- src/test.java
+++ src/test.java
@@ -8,6 +8,6 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hallo Git!!!/nfunktioniert auch der eclipse account?");
+		System.out.println("Hallo Git!!!/nfunktioniert auch der eclipse account?edit");
 	}
 }
