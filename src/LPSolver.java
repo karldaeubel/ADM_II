4dc8977071a700f2ec8a588aa92c870b625f99f1
Karl Daeubel <karl_daeubel@web.de>
2011-12-20 20:08:13 +0100

    angepasste LPSolver klasse mit Matrix als elemente

--- src/LPSolver.java
+++ src/LPSolver.java
@@ -2,18 +2,19 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 
 public class LPSolver {
 	
 	private LPReader lp;
 	
-	private FracBigInt[][] Tableau;
+	private Matrix A;
 	
-	private FracBigInt[][] Carry;
+	private Matrix Carry;
 	
 	public LPSolver() {
-		System.out.println("----------Start the Programm----------");
+		System.out.println("----------Start the Program----------");
 		lp = new LPReader("/home/karl/Desktop/afiro.lp", false);
 		try {	
 				long start = System.nanoTime();
@@ -30,7 +31,7 @@ public class LPSolver {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		Tableau = new FracBigInt[0][0];
+		A = new Matrix(new FracBigInt[0][0]);
 	}
 	
 	public void solve() {
@@ -42,12 +43,12 @@ public class LPSolver {
 			for(int i = 0; i < c.length; i++) {
 				c[i] = new FracBigInt(lp.objectiveVector()[i]);
 			}
-			FracBigInt[] optSolution = PhaseII(c , feasibleX);
+			FracBigInt[] optSolution = PhaseII();
 			System.out.println(Arrays.toString(optSolution));
 		}
 	}
 
-	private FracBigInt[] PhaseI() {
+	private Matrix PhaseI() {
 		int m = lp.noOfConstraints();
 		int noOfGE = 0;
 		int noOfLE = 0;
@@ -65,92 +66,88 @@ public class LPSolver {
 			}
 		}
 		int n = m + lp.noOfVariables() + noOfGE + noOfLE;
-		System.out.println(noOfEq + "  " + noOfGE + "  " + noOfLE + "  " + lp.noOfConstraints() + "  " + lp.noOfVariables());
-		Tableau = new FracBigInt[m +2][n +1];
+		
+		A = new Matrix(new FracBigInt[m +1][n +1]);
 		int r1 = 0;
 		FracBigInt sum = new FracBigInt("0");
-		for(int j = 0; j < n +1; j++) {
-			for(int i = 0; i <  m +2; i++) {
-				Tableau[i][j] = new FracBigInt("0");
-				if(i == 0 && j >= m +1) {
-					Tableau[i][j] = new FracBigInt("1");
-				}else if(i == 1 && j > 0 && j < m +1) {
-					Tableau[i][j] = new FracBigInt("1");
-				}else if(i > 1 && j == 0) {
-					Tableau[i][j] = new FracBigInt(lp.rhs[i -2]);
-					sum = sum.substract(Tableau[i][j]);
-				}else if(i > 1 && j > 0 && j < m +1 && i == j +1) {
-					Tableau[i][j] = new FracBigInt("1");
-					sum = sum.substract(Tableau[i][j]);
-				}else if(i > 1 && j >= m +1 && j < m +1 +lp.noOfVariables()) {
-					Tableau[i][j] = new FracBigInt(lp.constraint[i -2][j - m -1]);
-					sum = sum.substract(Tableau[i][j]);
-				}else if(i > 1 && j >= m +1 +lp.noOfVariables() && r[i -2] != 0 && r1 == 0) {
-					Tableau[i][j] = new FracBigInt(r[i -2]);
+		FracBigInt[][] unit = new FracBigInt[m][m];
+		FracBigInt[][] b = new FracBigInt[m][1];
+		FracBigInt[][] a = new FracBigInt[m][lp.noOfVariables() + noOfGE + noOfLE];
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < m; j++) {
+				if(i == j) {
+					unit[i][j] = new FracBigInt("1"); 
+				}else {
+					unit[i][j] = new FracBigInt("0"); 
+				}
+			}
+			A.set(0,i +1, new FracBigInt("0"));
+			b[i][0] = new FracBigInt(lp.rhs[i]);
+			sum = sum.substract(b[i][0]);
+		}
+		A.set(0, 0, sum);
+		sum = new FracBigInt("0");
+		for(int j = 0; j < lp.noOfVariables() + noOfGE + noOfLE; j++) {
+			for(int i = 0; i < m;i++) {
+				if(j < lp.noOfVariables()) {
+					a[i][j] = new FracBigInt(lp.constraint[i][j]);
+					sum = sum.substract(a[i][j]);
+				}else if( r[i -2] != 0 && r1 == 0) {
+					a[i][j] = new FracBigInt(r[i -2]);
 					r1 = 1;
 					r[i -2] = 0;
-					sum = sum.substract(Tableau[i][j]);
+					sum = sum.substract(a[i][j]);
 				}
 			}
 			r1 = 0;
-			Tableau[1][j] = Tableau[1][j].add(sum);
+			A.set(0,j + m +1,sum);
 			sum = new FracBigInt("0");
 		}
-		System.out.println(Arrays.deepToString(Tableau));
-
 		
-		FracBigInt[] c = new FracBigInt[n];
-		FracBigInt[] feasible = new FracBigInt[n];
-		for(int i = 0; i < n; i++) {
-			if(i < m) {
-				c[i] = new FracBigInt("1");
-				feasible[i] = new FracBigInt(lp.rhs[i]);
-			}else {
-				c[i] = new FracBigInt("0");
-				feasible[i] = new FracBigInt("0");
-			}
-		}
-		System.out.println(Arrays.toString(feasible) + "  " + Arrays.toString(c));
-		FracBigInt[] x = PhaseII(c, feasible);
+		A.set(1, m, 1, m, new Matrix(unit));
+		A.set(0, 1, 1, m, new Matrix(b));
+		A.set(1, m, 1 +m, n +1, new Matrix(a));
+		
+		System.out.println(A);
+		int[] B = new int[m];
+		int[] N = new int[n -m];
 		for(int i = 0; i < m; i++) {
-			if(x[i] != FracBigInt.ZERO) {return null;}
+			B[i] = i;
 		}
+		for(int i = 0; i < n -m; i++) {
+			N[i] = m +i;
+		}
+		
+		
+		Matrix x = PhaseII(B,N);
+		/*for(int i = 0; i < m; i++) {
+			if(x[i] != FracBigInt.ZERO) {return null;}
+		}*/
 		return x;
 	}
 	
-	public FracBigInt[] PhaseII(FracBigInt[] c, FracBigInt[] x) {
+	public Matrix PhaseII(int[] B, int[] N) {
 		int m = lp.noOfConstraints();
-		int n = Tableau.length -1;
-		Carry = new FracBigInt[m +1][m +1];
-		boolean[] B = new boolean[n];
-		for(int i = 0; i < m; i++) {
-			B[i] = true;
-		}
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < m; j++) {
-				Carry[i][j] = Tableau[i][j].clone();
-			}
-		}
-		FracBigInt cN = FracBigInt.ZERO;
-		FracBigInt result = new FracBigInt("0");
-		int k = 1;
 		
-		while(k < n +1) {
-			if(B[k] == false) {
-				for(int i = 0; i < m; i++) {
-					result = result.add(Carry[0][i +1].add(Tableau[i +2][k]));
+		Carry = (Matrix) A.of(0, m +1, 0, m +1);
+
+		while(true) {
+			//Calculate the reduced cost c_j, j in N, and save the index k
+			int k = -1;
+			for(int j = 0; j < N.length; j++) {
+				//TODO korrekte implementierung dieses ausdrucks...
+				FracBigInt c_j = A.get(0,N[j]).add(Carry.of(0, 0, 1, m).multiply(A.of(1, m, N[j], N[j])).get(0,0));
+				if(c_j.compareTo(FracBigInt.ZERO) == -1) {
+					k = j;
+					break;
 				}
-				cN = Tableau[1][k].add(result);
-			}else {
-				k++;
 			}
-			if(cN.equals(FracBigInt.ZERO)) {
-				k--;
-				break;
+			if(k == -1) {
+				return (Matrix) A.of(1, m, 0, 0);
 			}
+			Matrix x = (Matrix) Carry.of(1, m, 1, m).multiply(A.of(1, m, k, k));
+			
 		}
-		if(k == n +1) {return x;}
-		
 		
 		return null;
 		
