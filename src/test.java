72361d1d62f4740bd2a33b43b3f1afab17465e54
Karl Daeubel <karl_daeubel@web.de>
2011-12-22 00:24:50 +0100

    test 3

---
+++ src/test.java
@@ -0,0 +1,14 @@
+/**
+ * @author Karl Daeubel
+ * A class for testing the GitHub-Repository with Eclipse and EGit
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LPSolver lp = new LPSolver();
+		lp.solve();
+	}
+}
