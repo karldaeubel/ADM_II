3442b612bb6d8d465d0ddba19108662a333d5309
marcelmerkle <marcelmerkle@loopback>
2011-12-22 16:21:37 +0100

    argmin korrigiert

--- src/Matrix.java
+++ src/Matrix.java
@@ -115,6 +115,8 @@ public class Matrix implements MatrixInterface {
 	@Override
 	public int[] argmin() {
 		int result[] = new int[2];
+		result[0]=-1;
+		result[1]=-1;
 		FracBigInt min = this.matrix[0][0];
 		for ( int i = 0 ; i<this.m ; i++ ){
 			for ( int j = 0 ; j<this.n ; j++ ){
