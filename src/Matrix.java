4a1cfb5daf1e5fff2bf2fcd8ec9fcc8e59081c12
marcelmerkle <marcelmerkle@loopback>
2012-01-06 22:02:12 +0100

    In der Matrix-Klasse gibt es jetzt eine Multiplikationsmethode, die die Berechnungen in verschiedene Threads aufteilt. Auf meinem Prozessor wurde eine Matrixmultiplikation 2-3 mal schneller ausgeführt.

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 
 public class Matrix implements MatrixInterface {
@@ -23,6 +24,18 @@ public class Matrix implements MatrixInterface {
 		for ( int i=0 ; i<m ; i++ ){
 			for ( int j=0 ; j<n ; j++ ){
 				this.matrix[i][j] = matrix[i][j];
+			}
+		}
+	}
+	
+	public Matrix(int r, int m, int n){
+		matrix = new FracBigInt[m][n];
+		this.m = m;
+		this.n = n;
+		Random rand = new Random();
+		for ( int i = 0; i<m ; i++ ){
+			for ( int j = 0 ; j<n ; j++ ){
+				matrix[i][j] = new FracBigInt(rand.nextInt(10));
 			}
 		}
 	}
@@ -372,6 +385,103 @@ public class Matrix implements MatrixInterface {
 		return r-1;
 	}
 	
+	public double[][] toDouble(){
+		double[][] result = new double[this.m][this.n];
+		for ( int i = 0 ; i < this.m ; i++){
+			for( int j = 0; j < this.n ; j++){
+				result[i][j] = this.get(i, j).toDouble();
+			}
+		}
+		return result;
+	}
+	
+	public Matrix altMultiply(Matrix A){
+		Matrix result = new Matrix(this.m , A.getN());
+		for ( int i = 0 ; i < this.m ; i=i+10){
+			MatrixMult mult1 = new MatrixMult(A,i,(Matrix) this.of(i,i,0,A.getN()-1),result);
+			mult1.start();
+			MatrixMult mult2 = null;
+			MatrixMult mult3 = null;
+			MatrixMult mult4 = null;
+			MatrixMult mult5 = null;
+			MatrixMult mult6 = null;
+			MatrixMult mult7 = null;
+			MatrixMult mult8 = null;
+			MatrixMult mult9 = null;
+			MatrixMult mult10 = null;
+			if ( i+1 < this.m ){
+				mult2 = new MatrixMult(A,i+1,(Matrix) this.of(i+1,i+1,0,A.getN()-1),result);
+				mult2.start();
+			}
+			if ( i+2 < this.m ){
+				mult3 = new MatrixMult(A,i+2,(Matrix) this.of(i+2,i+2,0,A.getN()-1),result);
+				mult3.start();
+			}
+			if ( i+3 < this.m ){
+				mult4 = new MatrixMult(A,i+3,(Matrix) this.of(i+3,i+3,0,A.getN()-1),result);
+				mult4.start();
+			}
+			if ( i+4 < this.m ){
+				mult5 = new MatrixMult(A,i+4,(Matrix) this.of(i+4,i+4,0,A.getN()-1),result);
+				mult5.start();
+			}
+			if ( i+5 < this.m ){
+				mult6 = new MatrixMult(A,i+5,(Matrix) this.of(i+5,i+5,0,A.getN()-1),result);
+				mult6.start();
+			}
+			if ( i+6 < this.m ){
+				mult7 = new MatrixMult(A,i+6,(Matrix) this.of(i+6,i+6,0,A.getN()-1),result);
+				mult7.start();
+			}
+			if ( i+7 < this.m ){
+				mult8 = new MatrixMult(A,i+7,(Matrix) this.of(i+7,i+7,0,A.getN()-1),result);
+				mult8.start();
+			}
+			if ( i+8 < this.m ){
+				mult9 = new MatrixMult(A,i+8,(Matrix) this.of(i+8,i+8,0,A.getN()-1),result);
+				mult9.start();
+			}
+			if ( i+9 < this.m ){
+				mult10 = new MatrixMult(A,i+9,(Matrix) this.of(i+9,i+9,0,A.getN()-1),result);
+				mult10.start();
+			}
+			
+			try {
+				mult1.join();
+				if ( mult2 != null ){
+					mult2.join();
+				}
+				if ( mult3 != null ){
+					mult3.join();
+				}
+				if ( mult4 != null ){
+					mult4.join();
+				}
+				if ( mult5 != null ){
+					mult5.join();
+				}
+				if ( mult6 != null ){
+					mult5.join();
+				}
+				if ( mult7 != null ){
+					mult5.join();
+				}
+				if ( mult8 != null ){
+					mult5.join();
+				}
+				if ( mult9 != null ){
+					mult5.join();
+				}
+				if ( mult10 != null ){
+					mult5.join();
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 	public static void main(String[] args){
 		/*
 		FracBigInt[][] test = {{new FracBigInt("1"),new FracBigInt("2")},{new FracBigInt("3"),new FracBigInt("4")}};
@@ -393,7 +503,7 @@ public class Matrix implements MatrixInterface {
 		System.out.println(testmatrix.add(testmatrix2));
 		testmatrix.remove(1, 0);
 		System.out.println(testmatrix);
-		*/
+		
 		
 		FracBigInt[][] carryarray = {{FracBigInt.ZERO,FracBigInt.ZERO,FracBigInt.ZERO,FracBigInt.ZERO},{new FracBigInt("10"),FracBigInt.ONE,FracBigInt.ZERO,FracBigInt.ZERO},{new FracBigInt("8"),FracBigInt.ZERO,FracBigInt.ONE,FracBigInt.ZERO},{new FracBigInt("24"),FracBigInt.ZERO,FracBigInt.ZERO,FracBigInt.ONE}};
 		Matrix carry = new Matrix(carryarray);
@@ -401,12 +511,29 @@ public class Matrix implements MatrixInterface {
 		Matrix x = new Matrix(xarray);
 		double[] bounds = {Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY};
 		int[] B = {2,3,4};
+		
 		System.out.println(carry);
 		System.out.println(x);
 		System.out.println(carry.step(B,x,bounds));
 		System.out.println(carry);
-		
-		
+		*/
+		Matrix test1 = new Matrix(1,300,300);
+		Matrix test2 = new Matrix(1,300,300);
+		Matrix test3 = null;
+		long time1;
+		long time2;
+		//System.out.println(test1);
+		//System.out.println(test2);
+		time1=System.nanoTime();
+		//System.out.println(test1.multiply(test2));
+		test3 = (Matrix) test1.multiply(test2);
+		time2 = System.nanoTime();
+		System.out.println("normal: "+(time2-time1));
+		time1 = System.nanoTime();
+		//System.out.println(test1.altMultiply(test2));
+		test3 = test1.altMultiply(test2);
+		time2 = System.nanoTime();
+		System.out.println("thread: "+(time2-time1));
 	}
 
 	
