bdaf372db70dc40b2b49c3d9916e38a09cc6ebaa
marcelmerkle <marcelmerkle@loopback>
2012-01-07 10:51:52 +0100

    kleiner Fehler in der neuen Matrixmultiplikation berichtigt.

--- src/Matrix.java
+++ src/Matrix.java
@@ -412,68 +412,68 @@ public class Matrix implements MatrixInterface {
 			if ( i+1 < this.m ){
 				mult2 = new MatrixMult(A,i+1,(Matrix) this.of(i+1,i+1,0,A.getN()-1),result);
 				mult2.start();
-			}
-			if ( i+2 < this.m ){
-				mult3 = new MatrixMult(A,i+2,(Matrix) this.of(i+2,i+2,0,A.getN()-1),result);
-				mult3.start();
-			}
-			if ( i+3 < this.m ){
-				mult4 = new MatrixMult(A,i+3,(Matrix) this.of(i+3,i+3,0,A.getN()-1),result);
-				mult4.start();
-			}
-			if ( i+4 < this.m ){
-				mult5 = new MatrixMult(A,i+4,(Matrix) this.of(i+4,i+4,0,A.getN()-1),result);
-				mult5.start();
-			}
-			if ( i+5 < this.m ){
-				mult6 = new MatrixMult(A,i+5,(Matrix) this.of(i+5,i+5,0,A.getN()-1),result);
-				mult6.start();
-			}
-			if ( i+6 < this.m ){
-				mult7 = new MatrixMult(A,i+6,(Matrix) this.of(i+6,i+6,0,A.getN()-1),result);
-				mult7.start();
-			}
-			if ( i+7 < this.m ){
-				mult8 = new MatrixMult(A,i+7,(Matrix) this.of(i+7,i+7,0,A.getN()-1),result);
-				mult8.start();
-			}
-			if ( i+8 < this.m ){
-				mult9 = new MatrixMult(A,i+8,(Matrix) this.of(i+8,i+8,0,A.getN()-1),result);
-				mult9.start();
-			}
-			if ( i+9 < this.m ){
-				mult10 = new MatrixMult(A,i+9,(Matrix) this.of(i+9,i+9,0,A.getN()-1),result);
-				mult10.start();
+				if ( i+2 < this.m ){
+					mult3 = new MatrixMult(A,i+2,(Matrix) this.of(i+2,i+2,0,A.getN()-1),result);
+					mult3.start();
+					if ( i+3 < this.m ){
+						mult4 = new MatrixMult(A,i+3,(Matrix) this.of(i+3,i+3,0,A.getN()-1),result);
+						mult4.start();
+						if ( i+4 < this.m ){
+							mult5 = new MatrixMult(A,i+4,(Matrix) this.of(i+4,i+4,0,A.getN()-1),result);
+							mult5.start();
+							if ( i+5 < this.m ){
+								mult6 = new MatrixMult(A,i+5,(Matrix) this.of(i+5,i+5,0,A.getN()-1),result);
+								mult6.start();
+								if ( i+6 < this.m ){
+									mult7 = new MatrixMult(A,i+6,(Matrix) this.of(i+6,i+6,0,A.getN()-1),result);
+									mult7.start();
+									if ( i+7 < this.m ){
+										mult8 = new MatrixMult(A,i+7,(Matrix) this.of(i+7,i+7,0,A.getN()-1),result);
+										mult8.start();
+										if ( i+8 < this.m ){
+											mult9 = new MatrixMult(A,i+8,(Matrix) this.of(i+8,i+8,0,A.getN()-1),result);
+											mult9.start();
+											if ( i+9 < this.m ){
+												mult10 = new MatrixMult(A,i+9,(Matrix) this.of(i+9,i+9,0,A.getN()-1),result);
+												mult10.start();
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 			
 			try {
 				mult1.join();
 				if ( mult2 != null ){
 					mult2.join();
-				}
-				if ( mult3 != null ){
-					mult3.join();
-				}
-				if ( mult4 != null ){
-					mult4.join();
-				}
-				if ( mult5 != null ){
-					mult5.join();
-				}
-				if ( mult6 != null ){
-					mult5.join();
-				}
-				if ( mult7 != null ){
-					mult5.join();
-				}
-				if ( mult8 != null ){
-					mult5.join();
-				}
-				if ( mult9 != null ){
-					mult5.join();
-				}
-				if ( mult10 != null ){
-					mult5.join();
+					if ( mult3 != null ){
+						mult3.join();
+						if ( mult4 != null ){
+							mult4.join();
+							if ( mult5 != null ){
+								mult5.join();
+								if ( mult6 != null ){
+									mult6.join();
+									if ( mult7 != null ){
+										mult7.join();
+										if ( mult8 != null ){
+											mult8.join();
+											if ( mult9 != null ){
+												mult9.join();
+												if ( mult10 != null ){
+													mult10.join();
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -517,21 +517,21 @@ public class Matrix implements MatrixInterface {
 		System.out.println(carry.step(B,x,bounds));
 		System.out.println(carry);
 		*/
-		Matrix test1 = new Matrix(1,300,300);
-		Matrix test2 = new Matrix(1,300,300);
+		Matrix test1 = new Matrix(1,10,10);
+		Matrix test2 = new Matrix(1,10,10);
 		Matrix test3 = null;
 		long time1;
 		long time2;
 		//System.out.println(test1);
 		//System.out.println(test2);
 		time1=System.nanoTime();
-		//System.out.println(test1.multiply(test2));
-		test3 = (Matrix) test1.multiply(test2);
+		System.out.println(test1.multiply(test2));
+		//test3 = (Matrix) test1.multiply(test2);
 		time2 = System.nanoTime();
 		System.out.println("normal: "+(time2-time1));
 		time1 = System.nanoTime();
-		//System.out.println(test1.altMultiply(test2));
-		test3 = test1.altMultiply(test2);
+		System.out.println(test1.altMultiply(test2));
+		//test3 = test1.altMultiply(test2);
 		time2 = System.nanoTime();
 		System.out.println("thread: "+(time2-time1));
 	}
