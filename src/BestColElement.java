f062ace24ad5647473f0150e1ff5e09df6d0acf3
Karl Daeubel <karl_daeubel@web.de>
2012-01-15 20:34:20 +0100

    java doc

--- src/BestColElement.java
+++ src/BestColElement.java
@@ -1,25 +1,54 @@
-
-public class BestColElement {
-	
+/**
+ * a class for an element of the list in which are all elements with feasible reduced costs
+ * @author marcel, karl
+ *
+ */
+public class BestColElement implements Comparable<BestColElement>{
+	/**
+	 * the current index of the variable in A
+	 */
 	public int colIndex;
-	
+	/**
+	 * the current index in the array of all non basis variables
+	 */
 	public int nonBasisIndex;
-	
+	/**
+	 * the reduced costs of this column multiplied by theta
+	 */
 	public FracBigInt redcost;
-	
+	/**
+	 * a default constructor
+	 */
 	public BestColElement() {
 		colIndex = -1;
 		nonBasisIndex = -1;
 		redcost = FracBigInt.ZERO;
 	}
-	
+	/**
+	 * a constructor initialize an element of the best reduced cost list
+	 * @param ci the current column index in A
+	 * @param noni the current index in the non basis array
+	 * @param cost the current reduced costs multiplied by theta
+	 */
 	public BestColElement(int ci, int noni, FracBigInt cost) {
 		colIndex = ci;
 		nonBasisIndex = noni;
 		redcost = cost.clone();
 	}
 	
+	@Override
 	public String toString() {
 		return colIndex + " " + redcost;
 	}
+
+	@Override
+	public int compareTo(BestColElement temp) {
+		if(redcost.compareTo(temp.redcost) == 1) {
+			return 1;
+		}else if(redcost.compareTo(temp.redcost) == -1) {
+			return -1;
+		}else {
+			return 0;
+		}
+	}	
 }
--- src/NonBasis.java
+++ src/NonBasis.java
@@ -1,24 +1,43 @@
-
+/**
+ * a class to represent all non basis variables including whether they are in the upper or lower bound set
+ * @author marcel, karl
+ *
+ */
 public class NonBasis {
-	
+	/**
+	 * represent the lower bound set
+	 */
 	public static final int L = -1;
-	
+	/**
+	 * represent the upper bound set
+	 */
 	public static final int U = 1;
-	
+	/**
+	 * the index in the A matrix
+	 */
 	public int index;
-	
+	/**
+	 * is this non basis variable in the upper or lower bound set?
+	 */
 	public int LorU;
-	
+	/**
+	 * a default constructor for a non basis variable with index = -1 and it is set to non of the lower or upper sets 
+	 */
 	public NonBasis() {
 		index = -1;
 		LorU = 0;
 	}
-	
+	/**
+	 * a constructor to initialize a non basis variable
+	 * @param idx the current index of the variable in A
+	 * @param lu tells whether it is in the upper or lower bound set, have to be one of the static variables L or U
+	 */
 	public NonBasis(int idx, int lu) {
 		index = idx;
 		LorU = lu;
 	}
 	
+	@Override
 	public String toString() {
 		return (index + ":" + LorU);
 	}
